47d87a8c4d98dcd84ad10094e46ef82aa134983c
wvanamstel <wlvanamstel@gmail.com>
2014-12-07 09:15:02 -0800

    minute data

--- helper_classes.py
+++ helper_classes.py
@@ -11,8 +11,9 @@ class MinuteData(object):
         IN: all strings; symbol, interval in secs, time period, open/close/etc
         OUT: data frame with historical minute prices
     '''
-    def __init__(self, symbol, interval='60', period='1d', dat='d,o,h,l,c,v'):
-        self.symbol = symbol
+    def __init__(self, symbol, interval='60', period='2d', dat='d,o,h,l,c,v'):
+        self.symbol = symbol.upper()
+        self.interval = float(interval)
         self.url = 'http://www.google.com/finance/getprices?' + \
                    'i=' + interval + \
                    '&p=' + period + \
@@ -21,29 +22,43 @@ class MinuteData(object):
 
     def get_data(self):
         print 'Fetching data'
-        request = urllib2.Request(self.url)        
+        request = urllib2.Request(self.url)
         raw_data = urllib2.urlopen(request).readlines()
-        raw_data = raw_data[7:]
-        
-        quotes = np.zeros((len(raw_data), 5))
+        raw_data = raw_data[7:]  # strip headers from received data
+
         time_stamps = []
-        ind = 0
-        first_entry = raw_data[0].strip('\n').split(',')
-        base_stamp = first_entry[0].strip('a')
-        time_stamps.append(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(base_stamp))))
-        quotes[ind,:] = first_entry[1:]
-        for line in raw_data[1:]:            
+        quotes = []
+        for line in raw_data:
             line = line.strip('\n')
             line = line.split(',')
-            time_stamps.append(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(base_stamp) + 60*ind)))
-            ind += 1
-            quotes[ind,:] = line[1:]
-            
-        cols = ['Open', 'High', 'Low', 'Close', 'Volume']
-        quotes_out = pd.DataFrame(quotes, index = time_stamps, columns=cols)
-        
+            # set correct time stamp, the base stamp is in 'a1415277322' epoch
+            # format, for consecutive quotes  in the same day, there is an
+            # offset number
+            if list(line[0])[0] == 'a':
+                date_base = float(line[0].strip('a'))
+                date_offset = 0
+            else:
+                date_offset = float(line[0]) * self.interval
+
+            time_stamps.append(time.strftime('%Y-%m-%d %H:%M:%S',
+                                             time.localtime(date_base +
+                                             date_offset)))
+
+            prices = []
+            prices.extend([float(x) for x in line[1:]])
+            quotes.append(prices)
+
+        quotes = np.array(quotes)
+
+        cols_raw = ['Close', 'High', 'Low', 'Open', 'Volume']
+        quotes_out = pd.DataFrame(quotes, index = time_stamps, columns=cols_raw)
+        # rearrange columns to normal order
+        cols_target = ['Open', 'High', 'Low', 'Close', 'Volume']
+        quotes_out = quotes_out[cols_target]
+
         return quotes_out
-        
-if __name__== '__main__':
-    a = MinuteData('AA')
+
+if __name__ ==  '__main__':
+    a = MinuteData('MSFT')
     test = a.get_data()
+    
