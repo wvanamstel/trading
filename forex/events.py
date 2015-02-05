b3005c21309877e53c7c4bc34684603e17130784
wvanamstel <wlvanamstel@gmail.com>
2015-02-04 19:33:50 -0800

    fixed bug error classes

--- forex/events.py
+++ forex/events.py
@@ -27,3 +27,9 @@ class OrderEvent(Event):
         self.order_type = order_type
         self.side = side
         
+class SignalEvent(Event):
+    def __init__(self, instrument, order_type, side):
+        self.instrument = instrument
+        self.order_type = order_type
+        self.side =side
+        
